3bae32a3e7ad4162e5964b84695a667cda89f5bd
mhanda <mohammedhanda109@gmail.com>
2024-01-06 19:18:24 +0100

    bar navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import "./App.scss"
-import Navbar from "./chat/navbar/Navbar"
-import SideBar from "./chat/sideBar/SideBar";
+import Navbar from "./components/navbar/Navbar"
+import SideBar from "./components/sideBar/SideBar";
 
 const App = () => {
   return <div>
-    <section>
+    <section id="Homepage">
       <Navbar />
       <SideBar />
     </section>
-    <section>parallax</section>
-    <section>services</section>
-    <section>parallax</section>
+    <section id="Services">parallax</section>
+    <section >services</section>
+    <section id="Contact">parallax</section>
     <section>portfolio1</section>
-    <section>portfolio2</section>
+    <section id="About">portfolio2</section>
     <section>portfolio3</section>
-    <section>contact</section>
+    <section >contact</section>
   </div>;
 };
 
---
+++ src/components/sideBar/SideBar.jsx
@@ -0,0 +1,40 @@
+import { motion } from "framer-motion"
+import "./sidebar.scss"
+import ToggleButton from "./toggleButton/ToggleButton";
+import Links from "./Links/Links";
+import { useState } from "react";
+
+const SideBar = () => {
+
+  const [open, setOpen] = useState(false);
+
+  const variant = {
+    open: {
+      clipPath: 'circle(1200px at 50px 50px)',
+      transition: {
+        type: 'spring',
+        stiffness: 10,
+      }
+    },
+    closed: {
+      clipPath: 'circle(30px at 50px 50px)',
+      transition: {
+        delay: 0.2,
+        type: 'spring',
+        stiffness: 200,
+        damping: 40,
+      }
+    }
+  }
+
+  return (
+    <motion.div animate={open ? 'open' : 'closed'} className="sidebar">
+      <motion.div className="bg" variants={variant}>
+        <Links />
+      </motion.div>
+      <ToggleButton setOpen={setOpen} />
+    </motion.div>
+  );
+};
+
+export default SideBar;
